b10a2adcde267f2ff7a5814a02badfee8fbdc23b
ivanfsilva <ivanfs27@gmail.com>
2022-03-12 16:41:52 -0300

    initial test

--- src/test/java/testeinicial/TesteGoogle.java
+++ src/test/java/testeinicial/TesteGoogle.java
@@ -3,14 +3,26 @@ package testeinicial;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
 
     @Test
-    public void teste() {
+    public void testeFireFox() {
         System.setProperty("webdriver.gecko.driver", "D:\\Users\\ivanf\\drivers-selenium\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+
+        Assert.assertEquals("Google", driver.getTitle());
+
+        driver.quit();
+    }
+
+    @Test
+    public void testeChrome() {
+        System.setProperty("webdriver.gecko.driver", "D:\\Users\\ivanf\\drivers-selenium\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
 
         Assert.assertEquals("Google", driver.getTitle());
